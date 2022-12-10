84b52f242fe9a015bde6b5d234712e5964364a05
Michael Nascimento <michael.nsc@outlook.com>
2022-12-10 18:35:01 -0300

    remove console

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import styles from "./Button.module.scss";
 
 export const Button = ({ children }: { children: React.ReactNode }) => {
-  console.log(styles);
-
   return (
     <button className={styles.button} type="button">
       {children}
