e8044e4bd06474382aea9125c7a8d19d8adbfa61
Michael Nascimento <michael.nsc@outlook.com>
2022-12-13 06:04:07 -0300

    cria hook useIsMobile

---
+++ hooks/useIsMobile.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect, useCallback } from "react";
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const resizeListener = useCallback(() => {
+    setIsMobile(window.matchMedia(`(max-width: 1048px`).matches);
+  }, []);
+
+  useEffect(() => {
+    resizeListener();
+    window.addEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, [resizeListener]);
+
+  return isMobile;
+};
