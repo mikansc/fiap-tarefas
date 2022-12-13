7793a9f3316daa535b0f47fcf2eb48ce1183cfad
Michael Nascimento <michael.nsc@outlook.com>
2022-12-12 21:08:05 -0300

    cria authContext

---
+++ contexts/authContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { loginService } from "services/frontend/login-service";
+import { clearStorage, getFromStorage, setToStorage } from "services/frontend/storage-service";
+import type { ILoginCredentials, IUserResponse } from "types/User";
+
+const initialContext = {
+  isLoggedIn: false,
+  handleLogin: (credentials: ILoginCredentials) => {},
+  user: {} as IUserResponse,
+  handleLogout: () => {},
+};
+
+const authContext = createContext(initialContext);
+
+const AuthProvider = authContext.Provider;
+
+export const useAuth = () => {
+  const ctx = useContext(authContext);
+
+  return ctx;
+};
+
+export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUserResponse>({} as IUserResponse);
+
+  const isLoggedIn = Boolean(user.token && user.name && user.email);
+
+  useEffect(() => {
+    const userFromStorage = getFromStorage<IUserResponse>("usr");
+    if (userFromStorage) setUser(userFromStorage);
+  }, []);
+
+  const handleLogin = async (credentials: ILoginCredentials) => {
+    try {
+      const response = await loginService.signin(credentials);
+      setToStorage("usr", response);
+      setUser(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = () => {
+    clearStorage();
+    setUser({} as IUserResponse);
+  };
+
+  return <AuthProvider value={{ isLoggedIn, handleLogin, handleLogout, user }}>{children}</AuthProvider>;
+};
