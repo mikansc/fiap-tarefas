8570950aea225cc918803ed3a43326942e8f6183
Michael Nascimento <michael.nsc@outlook.com>
2022-12-15 15:35:10 -0300

    atualiza Datefield

--- components/DateField/DateField.tsx
+++ components/DateField/DateField.tsx
@@ -4,10 +4,12 @@ import { InputHTMLAttributes } from "react";
 
 import styles from "./DateField.module.scss";
 
-interface DateFieldProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface DateFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  fullWidth?: boolean;
+}
 
-export const DateField = forwardRef((props: DateFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
-  return <input {...props} className={styles.input} ref={ref} type="date" />;
+export const DateField = forwardRef(({ fullWidth = false, ...props }: DateFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
+  return <input {...props} className={`${styles.input} ${fullWidth ? styles.fullWidth : ""}`} ref={ref} type="date" />;
 });
 
 DateField.displayName = "InputField";
