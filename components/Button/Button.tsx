ba71ce52e219a953448a8b89d0e2da0741a0963c
Michael Nascimento <michael.nsc@outlook.com>
2022-12-15 15:34:58 -0300

    atualiza Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,11 +5,13 @@ import styles from "./Button.module.scss";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  fullWidth?: boolean;
+  variant?: "text" | "outlined";
 }
 
-export const Button = ({ children, ...rest }: ButtonProps) => {
+export const Button = ({ children, variant = "outlined", fullWidth = false, ...rest }: ButtonProps) => {
   return (
-    <button {...rest} className={styles.button} type="button">
+    <button {...rest} className={`${styles.button} ${styles[variant]} ${fullWidth ? styles.fullWidth : ""}`} type="button">
       {children}
     </button>
   );
