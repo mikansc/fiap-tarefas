8192203976011506e88218a68524cc2c6140072c
Michael Nascimento <michael.nsc@outlook.com>
2022-12-15 19:34:43 -0300

    renomeia InputField pra TextField

--- components/TextField/TextField.tsx
+++ components/TextField/TextField.tsx
@@ -3,16 +3,16 @@ import { forwardRef } from "react";
 import { ForwardedRef } from "react";
 import { InputHTMLAttributes } from "react";
 
-import styles from "./InputField.module.scss";
+import styles from "./TextField.module.scss";
 
-interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
   fullWidth?: boolean;
   variant?: "default" | "outlined";
   label?: string;
 }
 
-export const InputField = forwardRef((props: InputFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
+export const TextField = forwardRef((props: TextFieldProps, ref: ForwardedRef<HTMLInputElement>) => {
   const { icon, fullWidth = false, label, variant = "default", id, ...rest } = props;
 
   return (
@@ -20,10 +20,10 @@ export const InputField = forwardRef((props: InputFieldProps, ref: ForwardedRef<
       {label && <label htmlFor={id}>{label}</label>}
       <div className={styles.textfield}>
         {!!icon && icon}
-        <input {...rest} id={id} ref={ref} />
+        <input {...rest} id={id} ref={ref} type="text" />
       </div>
     </InputWrapper>
   );
 });
 
-InputField.displayName = "InputField";
+TextField.displayName = "InputField";
