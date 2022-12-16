ef1ee6c7e1c7654840d7f51c17e10f32895b772c
Michael Nascimento <michael.nsc@outlook.com>
2022-12-16 07:57:48 -0300

    move tasksContext para um nível mais baixo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import { AuthContextProvider } from "contexts/authContext";
-import { TasksContextProvider } from "contexts/tasksContext";
 import type { AppProps } from "next/app";
 import "../styles/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
-      <TasksContextProvider>
-        <Component {...pageProps} />
-      </TasksContextProvider>
+      <Component {...pageProps} />
     </AuthContextProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import { useAuth } from "contexts/authContext";
 
 import { Home, Login } from "containers";
+import { TasksContextProvider } from "contexts/tasksContext";
 
 export default function Homepage() {
   const { isLoggedIn } = useAuth();
-  return isLoggedIn ? <Home /> : <Login />;
+
+  return isLoggedIn ? (
+    <TasksContextProvider>
+      <Home />
+    </TasksContextProvider>
+  ) : (
+    <Login />
+  );
 }
