19042192759b50061f5195fe0e215eb6d8be538e
Michael Nascimento <michael.nsc@outlook.com>
2023-01-04 20:43:50 -0300

    adiciona toasts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,22 @@ import { useAuth } from "contexts/authContext";
 
 import { Home, Login } from "containers";
 import { TasksContextProvider } from "contexts/tasksContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Homepage() {
   const { isLoggedIn } = useAuth();
 
-  return isLoggedIn ? (
-    <TasksContextProvider>
-      <Home />
-    </TasksContextProvider>
-  ) : (
-    <Login />
+  return (
+    <>
+      {isLoggedIn ? (
+        <TasksContextProvider>
+          <Home />
+        </TasksContextProvider>
+      ) : (
+        <Login />
+      )}
+      <ToastContainer position={"bottom-center"} />
+    </>
   );
 }
--- services/frontend/http-fetch-client.ts
+++ services/frontend/http-fetch-client.ts
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { getHeaders } from "services/frontend/utils/get-headers";
 
 export class HttpFetchClient<T> {
@@ -36,8 +37,14 @@ export class HttpFetchClient<T> {
 async function handleResponse(res: Response) {
   const data = await res.json();
   if (res.ok) {
+    if (data.message) {
+      toast.success(data.message);
+    }
     return Promise.resolve(data);
   } else {
-    return Promise.reject(data);
+    if (data.error) {
+      toast.error(data.error);
+    }
+    return Promise.reject(data.error);
   }
 }
